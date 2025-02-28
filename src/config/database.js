82551c5f4be33292b3253d33ed275301faaf1e40
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-02-28 16:02:19 +0530

    buy

--- src/config/database.js
+++ src/config/database.js
@@ -4,13 +4,14 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.01/12701/Real_Estate",
-      useNewUrlParser =  true,
-      useUnifiedTopology =  true,
-     );
-    logger.info("MongoDB connected");
+    await mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/Real_Estate", {
+      useNewUrlParser: true,   // Correct way to pass options
+      useUnifiedTopology: true // Correct way to pass options
+    });
+
+    logger.info("✅ MongoDB connected successfully!");
   } catch (error) {
-    logger.error("MongoDB connection error:", error);
+    logger.error("❌ MongoDB connection error:", error);
     process.exit(1); // Exit process if DB connection fails
   }
 };
--- src/config/redis.js
+++ src/config/redis.js
@@ -3,8 +3,11 @@ const { logger } = require("../utils/logger");
 require("dotenv").config(); // Load environment variables
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379",
+  url: process.env.REDIS_URL || "redis://127.0.0.1:6379",
+  host: "127.0.0.1",
+  port: 6379
 });
+
 
 redisClient.on("error", (err) => {
   console.error("Redis error:", err);
